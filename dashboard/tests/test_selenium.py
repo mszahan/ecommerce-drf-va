2d727b8a58f2e8bfab6ca020436e9775d6f1c14d
mszahan <mszahan1@gmail.com>
2022-10-13 20:44:35 +0600

    added test files and pytest.ini and conftest.py

---
+++ dashboard/tests/test_selenium.py
@@ -0,0 +1,7 @@
+def test_example():
+    assert 'hola' == 'hola'
+
+
+# the file name that holds the tests need to be named with test prefix
+# like test_blah_blah.py
+# but you can name the test funtion name anything
